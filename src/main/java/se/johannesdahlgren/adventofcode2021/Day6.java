6d74ee8b225d28ae4d000d0ec38d18c5f90cc781
Johannes Dahlgren <johannes.dahlgren@gmail.com>
2021-12-06 20:41:52 +0100

    Day6 part2

--- src/main/java/se/johannesdahlgren/adventofcode2021/Day6.java
+++ src/main/java/se/johannesdahlgren/adventofcode2021/Day6.java
@@ -1,30 +1,42 @@
 package se.johannesdahlgren.adventofcode2021;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Optional;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Day6 {
 
-  private List<LanternFish> currentLanternFishes = new ArrayList<>();
+  private final List<Integer> initialInternalTimers;
 
   public Day6(String inputFile) {
-    List<Integer> initialInternalTimers = FileUtil.readCsvFileToIntList(inputFile);
-    for (Integer initialInternalTimer : initialInternalTimers) {
-      LanternFish lanternFish = new LanternFish(initialInternalTimer);
-      currentLanternFishes.add(lanternFish);
-    }
+    initialInternalTimers = FileUtil.readCsvFileToIntList(inputFile);
   }
 
-  public long spawnLanternFish(int daysToSimulate) {
+  public long spawnLanternFishSmarter(int daysToSimulate) {
+    Map<Integer, Long> timerCount = initialInternalTimers.stream()
+        .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
     for (int i = 0; i < daysToSimulate; i++) {
-      List<LanternFish> lanternFishesAtEndOfDay = new ArrayList<>(currentLanternFishes);
-      for (LanternFish lanternFish : currentLanternFishes) {
-        Optional<LanternFish> spawnedLanternFish = lanternFish.age();
-        spawnedLanternFish.ifPresent(lanternFishesAtEndOfDay::add);
+      Map<Integer, Long> nextTimerCount = new HashMap<>();
+
+      for (Entry<Integer, Long> entry : timerCount.entrySet()) {
+        if (entry.getKey() == 0) {
+          nextTimerCount.put(6, entry.getValue());
+          nextTimerCount.put(8, entry.getValue());
+        } else if (entry.getKey() == 7 && timerCount.containsKey(0)) {
+          nextTimerCount.put(entry.getKey() - 1, timerCount.get(0) + entry.getValue());
+        } else {
+          nextTimerCount.put(entry.getKey() - 1, entry.getValue());
+        }
       }
-      currentLanternFishes = lanternFishesAtEndOfDay;
+      timerCount = nextTimerCount;
     }
-    return currentLanternFishes.size();
+    return timerCount.values().stream()
+        .reduce(Long::sum)
+        .orElse(0L);
   }
+
 }
--- src/test/java/se/johannesdahlgren/adventofcode2021/Day6Test.java
+++ src/test/java/se/johannesdahlgren/adventofcode2021/Day6Test.java
@@ -10,22 +10,36 @@ class Day6Test {
   @Test
   void part1Example1() {
     Day6 day6 = new Day6("day6example.txt");
-    long numberOfLanternFishAfterDays = day6.spawnLanternFish(18);
+    long numberOfLanternFishAfterDays = day6.spawnLanternFishSmarter(18);
     assertThat(numberOfLanternFishAfterDays, Is.is(26L));
   }
 
   @Test
   void part1Example2() {
     Day6 day6 = new Day6("day6example.txt");
-    long numberOfLanternFishAfterDays = day6.spawnLanternFish(80);
+    long numberOfLanternFishAfterDays = day6.spawnLanternFishSmarter(80);
     assertThat(numberOfLanternFishAfterDays, Is.is(5934L));
   }
 
   @Test
   void part1() {
     Day6 day6 = new Day6("day6.txt");
-    long numberOfLanternFishAfterDays = day6.spawnLanternFish(80);
+    long numberOfLanternFishAfterDays = day6.spawnLanternFishSmarter(80);
     assertThat(numberOfLanternFishAfterDays, Is.is(373378L));
+  }
+
+  @Test
+  void part2example() {
+    Day6 day6 = new Day6("day6example.txt");
+    long numberOfLanternFishAfterDays = day6.spawnLanternFishSmarter(256);
+    assertThat(numberOfLanternFishAfterDays, Is.is(26984457539L));
+  }
+
+  @Test
+  void part2() {
+    Day6 day6 = new Day6("day6.txt");
+    long numberOfLanternFishAfterDays = day6.spawnLanternFishSmarter(256);
+    assertThat(numberOfLanternFishAfterDays, Is.is(1682576647495L));
   }
 
 }
